deb2b61f954f9a99a67d01b603be6d5049a6e764
ilenisnerd <41298678+ilenisnerd@users.noreply.github.com>
2019-03-24 19:06:22 +0400

    Add files via upload

---
+++ RockUpravlenie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockUpravlenie : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("a Pressed");
+            transform.Rotate(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("D Pressed");
+            transform.Rotate(-Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("S Pressed");
+        }
+    }
+}
